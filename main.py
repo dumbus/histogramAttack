569cf2fac9530cf783fe9e8ec86cde63d60d7d90
dumbus <dumbusx@gmail.com>
2023-11-21 18:07:12 +0300

    refactor: refactor get brightness matrix function

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from PIL import Image
 
 def main():
-    brightness_matrix = get_brightness_matrix()
-
-def get_brightness_matrix():
     image = Image.open('./imgs-06/6.bmp')
 
     width = image.size[0]
     height = image.size[1]
+
+    brightness_matrix = get_brightness_matrix(image, width, height)
+    # print_brightness_matrix(brightness_matrix)
+
+
+def get_brightness_matrix(image, width, height):
     pixels = image.load() # values of pixels of image
     
     matrix = []
@@ -19,6 +22,12 @@ def get_brightness_matrix():
         matrix.append(row)
 
     return matrix
+
+# def print_brightness_matrix(brightness_matrix):
+#     print("Brightness matrix for image:")
+
+#     for i in range (3):
+
 
 def get_histogram_data():
     im = Image.open('./imgs-06/6.bmp').convert('L')

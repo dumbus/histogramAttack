2923d1a1c5beb8dc9d69edbb355d3d8809620bc6
dumbus <dumbusx@gmail.com>
2023-11-21 17:24:41 +0300

    feat: add get_histogram_data function

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from PIL import Image
+
 def main():
     print("Hello world!")
+
+def get_histogram_data():
+    im = Image.open('./imgs-06/6.bmp').convert('L')
+    h = im.histogram()
+    # Print histogram
+    for idx, val in enumerate(h):
+        print(idx,val)
 
 if __name__ == '__main__':
     main()
